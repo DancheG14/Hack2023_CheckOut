756ed6effb2db2731f595e46251b5ef2b2239ceb
ZAYKIN DANIIL <92274248+DancheG14@users.noreply.github.com>
2023-01-21 05:21:06 +0300

    Update json_parser.py

--- json_parser.py
+++ json_parser.py
@@ -3,16 +3,20 @@ import re
 import json
 
 directory = 'hackaton_2023_01/task_3/full_dataset/json/'
+
 data = []
-for n in range(9999):
+
+for n in range(10000):
     name = str(n) + '.json'
     try:
         with open(directory + name, "r") as readFile:
             jsonData = json.load(readFile)
-        data.append(jsonData)
-    except FileNotFoundError:
+            jsonData["id"] = n
+            data.append(jsonData)
+    except ValueError:
         continue
+
+data
 
 with open('merged_data.json', "w") as outfile:
     json.dump(data, outfile)
-
